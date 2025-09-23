b561e35d07795b9ce21862dea8c98d603dcc7a44
Hafied <darka433@gmail.com>
2025-09-23 12:12:07 +0700

    fix some issue in track visitor

--- app/Http/Middleware/TrackVisitor.php
+++ app/Http/Middleware/TrackVisitor.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use App\Models\visitor;
+use App\Models\Visitor;
 use Symfony\Component\HttpFoundation\Response;
 
 class TrackVisitor
--- app/Models/visitor.php
+++ app/Models/visitor.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 
-class visitor extends Model
+class Visitor extends Model
 {
     use HasFactory;
     protected $fillable = ['ip_address', 'visited_at'];
